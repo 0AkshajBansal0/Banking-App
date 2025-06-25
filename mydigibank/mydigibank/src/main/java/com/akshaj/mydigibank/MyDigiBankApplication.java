c4b94398057dfb9c527de8de03db26b0b66cc867
Akshaj Bansal <161148217+0AkshajBansal0@users.noreply.github.com>
2025-06-25 11:16:12 +0530

    Feat: SpringBoot Structure

---
+++ mydigibank/mydigibank/src/main/java/com/akshaj/mydigibank/MyDigiBankApplication.java
@@ -0,0 +1,13 @@
+package com.akshaj.mydigibank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDigiBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDigiBankApplication.class, args);
+	}
+
+}
